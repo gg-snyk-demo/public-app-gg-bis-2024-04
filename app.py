d46b05ca764313007a67891dd9c05ce83f602872
Carol Danvers <carol-danvers-snyk@protonmail.com>
2024-05-03 23:03:57 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import antigravity
 
-SEED=0c5f9fac-cad2-4bcb-9aa5-5c39fa9d4204
+CERT="""-----BEGIN EC PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-256-CBC,4C46C97395A3286F6F3EFC98A7391202
+
+AgRJXtYBk+8pZ4zVxfPJqP2auTkgE44xzmLoVkzXZcy0R3TJcggUBwkW1D+ltcyd
+hiw8N+5MSTIo982FGtTJADIELx+NT43WGMz/2p0Mr7WFFI9sOiwItpcBlznjyrBb
+9sxCqkMr0BWIOp8wAeZhLpxGDrcQc6baAIwGVNCYU7gSnFL4ySAmhNMm940zEVxz
+JdAmPqWguiAk15Nyi1EH8i3yVsaAVH5p9q/GisCsx2I=
+-----END EC PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
